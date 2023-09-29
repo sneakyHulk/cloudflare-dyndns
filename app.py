66faa675d64002d1914352b92a2d786c467eb5a8
Lukas <sneakyHulk@users.noreply.github.com>
2023-09-29 18:39:37 +0200

    Update app.py

--- app.py
+++ app.py
@@ -16,20 +16,20 @@ def main():
     cf = CloudFlare.CloudFlare(token=token)
 
     if not token:
-        print('Missing token URL parameter.')
+        print('Missing token URL parameter.', file=sys.stderr)
         return flask.jsonify({'status': 'error', 'message': 'Missing token URL parameter.'}), 400
     if not zone:
-        print('Missing zone URL parameter.')
+        print('Missing zone URL parameter.', file=sys.stderr)
         return flask.jsonify({'status': 'error', 'message': 'Missing zone URL parameter.'}), 400
     if not ipv4 and not ipv6:
-        print('Missing ipv4 or ipv6 URL parameter.')
+        print('Missing ipv4 or ipv6 URL parameter.', file=sys.stderr)
         return flask.jsonify({'status': 'error', 'message': 'Missing ipv4 or ipv6 URL parameter.'}), 400
 
     try:
         zones = cf.zones.get(params={'name': zone})
 
         if not zones:
-            print('Zone {} does not exist.'.format(zone))
+            print('Zone {} does not exist.'.format(zone), file=sys.stderr)
             return flask.jsonify({'status': 'error', 'message': 'Zone {} does not exist.'.format(zone)}), 404
 
         a_record = cf.zones.dns_records.get(zones[0]['id'], params={
@@ -38,32 +38,33 @@ def main():
                                                'name': '{}'.format(zone), 'match': 'all', 'type': 'AAAA'})
 
         if ipv4 is not None and not a_record:
-            print('A record for {} does not exist.'.format(zone))
+            print('A record for {} does not exist.'.format(zone), file=sys.stderr)
             return flask.jsonify({'status': 'error', 'message': 'A record for {} does not exist.'.format(zone)}), 404
 
         if ipv6 is not None and not aaaa_record:
-            print('AAAA record for {} does not exist.'.format(zone))
+            print('AAAA record for {} does not exist.'.format(zone), file=sys.stderr)
             return flask.jsonify({'status': 'error', 'message': 'AAAA record for {} does not exist.'.format(zone)}), 404
 
         if ipv4 is not None and a_record[0]['content'] != ipv4:
-            print('name:', a_record[0]['name'], 'type: A', 'content:', ipv4, 'proxied:', a_record[0]['proxied'], 'ttl:', a_record[0]['ttl'])
+            print('name:', a_record[0]['name'], 'type: A', 'content:', ipv4, 'proxied:', a_record[0]['proxied'], 'ttl:', a_record[0]['ttl'], file=sys.stderr)
             cf.zones.dns_records.put(zones[0]['id'], a_record[0]['id'], data={
                                      'name': a_record[0]['name'], 'type': 'A', 'content': ipv4, 'proxied': a_record[0]['proxied'], 'ttl': a_record[0]['ttl']})
 
         if ipv6 is not None and aaaa_record[0]['content'] != ipv6:
-            print('name:', aaaa_record[0]['name'], 'type: AAAA', 'content:', ipv6, 'proxied:', aaaa_record[0]['proxied'], 'ttl:', aaaa_record[0]['ttl'])
+            print('name:', aaaa_record[0]['name'], 'type: AAAA', 'content:', ipv6, 'proxied:', aaaa_record[0]['proxied'], 'ttl:', aaaa_record[0]['ttl'], file=sys.stderr)
             cf.zones.dns_records.put(zones[0]['id'], aaaa_record[0]['id'], data={
                                      'name': aaaa_record[0]['name'], 'type': 'AAAA', 'content': ipv6, 'proxied': aaaa_record[0]['proxied'], 'ttl': aaaa_record[0]['ttl']})
     except CloudFlare.exceptions.CloudFlareAPIError as e:
-        print(str(e))
+        print(str(e), file=sys.stderr)
         return flask.jsonify({'status': 'error', 'message': str(e)}), 500
 
-    print('Update successful.')
+    print('Update successful.', file=sys.stderr)
     return flask.jsonify({'status': 'success', 'message': 'Update successful.'}), 200
 
 
 @app.route('/healthz', methods=['GET'])
 def healthz():
+    print('Health check!', file=sys.stderr)
     return flask.jsonify({'status': 'success', 'message': 'OK'}), 200
 
 
